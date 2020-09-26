738febb8e43813486a7e2c6f9cb97bc46c3f6ee5
Felícito Escobar <felicito.escobar@outlook.com>
2020-09-26 14:35:03 -0600

    Version inicial con definicion de alarmas

---
+++ voice_cn_pa.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file voice_cn_pa.hpp
+ * @author Felícito Manzano (felicito.manzano@detektor.com.sv)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-26
+ * 
+ */
+
+#ifndef __VOICE_CN_PA_HPP
+#define __VOICE_CN_PA_HPP
+
+    #define VOICE_WELLCOME                          1
+    #define VOICE_DRIVER_AUTHORIZED                 2
+    #define VOICE_DRIVER_UNAUTHORIZED               3
+    #define VOICE_DRIVER_SEATBELT_FASTENED          4
+    #define VOICE_DRIVER_SEATBELT_UNFASTENED        5
+    #define VOICE_COPILOT_SEATBELT_FASTENED         6
+    #define VOICE_COPILOT_SEATBELT_UNFASTENED       7
+    #define VOICE_CREW_SEATBELT_FASTENED            8
+    #define VOICE_CREW_SEATBELT_UNFASTENED          9
+    #define VOICE_WARNING_OVERSPEED                 10
+    #define VOICE_OVERSPEED_ALERT                   11
+    #define VOICE_WARNING_OVERSPEED_WET             10
+    #define VOICE_OVERSPEED_ALERT_WET               11
+    #define VOICE_HARSH_ACCELERATION                12
+    #define VOICE_HARSH_BREAKING                    13
+    #define VOICE_SHARP_CORNERING                   14
+    #define VOICE_CRASH_DETECTION                   15
+    #define VOICE_DOOR_SAFETY_REMINDER              16
+    #define VOICE_IDLE_TIME_WARNING                 17
+    #define VOICE_IDLE_ENGINE_SHUTDOWN              18
+    #define VOICE_TRACK_CONDITION_WET               19
+    #define VOICE_TRACK_CONDITION_DRY               20
+    #define VOICE_FINGERPRINT_ENABLE                21
+    #define VOICE_FINGERPRINT_DISABLE               22
+    #define VOICE_FINGERPRINT_CAPTURE_NEW           23
+    #define VOICE_FINGERPRINT_UP                    24
+    #define VOICE_FINGERPRINT_DOWN                  25
+    #define VOICE_FINGERPRINT_NEW_SUCCESSFUL        26
+    #define VOICE_FINGERPRINT_NEW_ERROR             27
+    #define VOICE_SAFETY_TIP1                       28
+    #define VOICE_SAFETY_TIP2                       29
+    #define VOICE_SAFETY_TIP3                       30
+
+#endif // __VOICE_CN_PA_HPP
